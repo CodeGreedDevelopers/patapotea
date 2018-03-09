a21b087a35c57852e0fa6530a81a0b55819ba7e2
Augustine <augustinetreezy@outlook.com>
2018-03-09 15:26:07 +0300

    Login activity added

--- app/src/main/java/com/codegreeddevelopers/patapotea/LoginActivity.java
+++ app/src/main/java/com/codegreeddevelopers/patapotea/LoginActivity.java
@@ -1,13 +1,38 @@
 package com.codegreeddevelopers.patapotea;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    Button user,pickup_point;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        user=findViewById(R.id.user);
+        pickup_point=findViewById(R.id.pickup_point);
+
+        user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        pickup_point.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,PickPointMainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/codegreeddevelopers/patapotea/PickPointMainActivity.java
@@ -0,0 +1,13 @@
+package com.codegreeddevelopers.patapotea;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PickPointMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_point_main);
+    }
+}
